ef5fd6ff6e3ea65279d72cbb82d9852bd6e314ea
Yuppich <81874089+Yuppich@users.noreply.github.com>
2021-04-26 01:14:00 +0300

    Adjustments
    Changed countdown to vanilla JS because of the hamburger menu conflict

--- js/countdown.js
+++ js/countdown.js
@@ -1,11 +1,13 @@
-const $ = elem => document.querySelector(elem);
+//const $ = elem => document.querySelector(elem);
+//console.log(selector)
 
-const countdown = function(_config) {
-  const tarDate = $(_config.target).getAttribute('data-date').split('-');
+
+const countdown = function (_config) {
+  const tarDate = document.getElementById('countdown').getAttribute('data-date').split('-'); /*$(_config.target).getAttribute('data-date').split('-');*/
   const day = parseInt(tarDate[0]);
   const month = parseInt(tarDate[1]);
   const year = parseInt(tarDate[2]);
-  let tarTime = $(_config.target).getAttribute('data-time');
+  let tarTime = document.getElementById('countdown').getAttribute('data-time'); /*$(_config.target).getAttribute('data-time');*/
   let tarhour, tarmin;
 
   if (tarTime != null) {
@@ -29,10 +31,11 @@ const countdown = function(_config) {
   // Set the date we're counting down to
   const countDownDate = new Date(year, month-1, day, tarhour, tarmin, 0, 0).getTime();
 
-  $(_config.target+' .day .word').innerHTML = _config.dayWord;
-  $(_config.target+' .hour .word').innerHTML = _config.hourWord;
-  $(_config.target+' .min .word').innerHTML = _config.minWord;
-  $(_config.target+' .sec .word').innerHTML = _config.secWord; 
+
+  //$(_config.target + ' .day .word').innerHTML = _config.dayWord;
+  //$(_config.target+' .hour .word').innerHTML = _config.hourWord;
+  //$(_config.target+' .min .word').innerHTML = _config.minWord;
+  //$(_config.target+' .sec .word').innerHTML = _config.secWord; 
 
   const updateTime = () => {
     // Get todays date and time
@@ -49,10 +52,14 @@ const countdown = function(_config) {
 
     requestAnimationFrame(updateTime);
 
-    $(_config.target+' .day .num').innerHTML = addZero(days);
-    $(_config.target+' .hour .num').innerHTML = addZero(hours);
-    $(_config.target+' .min .num').innerHTML = addZero(minutes);
-    $(_config.target+' .sec .num').innerHTML = addZero(seconds);
+      //$(_config.target + ' .day .num').innerHTML = addZero(days);
+     document.getElementById('day').innerHTML = addZero(days);
+      //$(_config.target + ' .hour .num').innerHTML = addZero(hours);
+     document.getElementById('hour').innerHTML = addZero(hours);
+     // $(_config.target + ' .min .num').innerHTML = addZero(minutes);
+     document.getElementById('min').innerHTML = addZero(minutes);
+      //$(_config.target + ' .sec .num').innerHTML = addZero(seconds);
+     document.getElementById('sec').innerHTML = addZero(seconds);
 
     // If the count down is over, write some text
     if (distance < 0) {
