b3a131ea18b87a4952e26902cd282ee50179cad9
Yuppich <81874089+Yuppich@users.noreply.github.com>
2021-04-30 00:08:26 +0300

    adjustments

--- js/main.js
+++ js/main.js
@@ -33,7 +33,21 @@ MAIN = {
 					}, 500)
 			}
 			)),
-			$(window).width() < 580 && $(".faq h2").html("FAQ");
+			$(window).scroll(function () {
+				if ($(this).scrollTop() > 50) {
+					$('#back-to-top').fadeIn();
+				} else {
+					$('#back-to-top').fadeOut();
+				}
+			});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+		$(window).width() < 580 && $(".faq h2").html("FAQ");
 	}
 }
 
